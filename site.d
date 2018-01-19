abb4ec1791c4bcada2e733a2de2a195037fffdaa
lei <lei@lefrak-a-25.umd.edu>
2018-01-19 17:29:36 -0500

    1
    1

--- site.d
+++ site.d
@@ -1,3 +1,3 @@
 site.o site.d : site.cc edmodels.h site.h patch.h cohort.h disturbance.h \
  restart.h /apps/BerkeleyDB/4.6.21NC/include/db_cxx.h \
- /apps/BerkeleyDB/4.6.21NC/include/db.h read_site_data.h landuse.h
+ /apps/BerkeleyDB/4.6.21NC/include/db.h read_site_data.h
