e35213cd01e7a3d7476cf2273c320f7c48e0da08
lei <lei@lefrak-a-25.umd.edu>
2018-02-15 12:33:26 -0500

    Switch to LU
    Change io, turn on LANDUSE

--- read_site_data.d
+++ read_site_data.d
@@ -1,2 +1,2 @@
 read_site_data.o read_site_data.d : read_site_data.cc /apps/netcdf/4.1.3/include/netcdf.h \
- edmodels.h site.h miami.h fire.h read_site_data.h
+ edmodels.h site.h miami.h landuse.h fire.h read_site_data.h
